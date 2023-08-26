6b9c52a4d24b4418a76a34ffdb4d6b8903bc7c21
Ahmad Syahbandi <saboonlbk@gmail.com>
2023-08-26 11:17:08 +0800

    tambah footer

--- src/components/Dashboard/Footer.tsx
+++ src/components/Dashboard/Footer.tsx
@@ -1,5 +1,12 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="mt-auto bg-white p-5 text-right font-roboto">
+      &copy;{new Date().getFullYear()} KongPos dari{" "}
+      <a href="https://www.misterkong.com" target="_blank" rel="noreferrer">
+        <strong> Misterkong</strong>
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -3,12 +3,13 @@ import Navbar from "../../components/Dashboard/Navbar";
 import Sidebar from "../../components/Dashboard/Sidebar";
 import { useRecoilValue } from "recoil";
 import { sidebarState } from "../../atom/sidebar";
+import Footer from "../../components/Dashboard/Footer";
 
 const Dashboard = () => {
   const isAktif = useRecoilValue(sidebarState);
   return (
     <div
-      className={`bg-gray-200 min-h-screen transition-all ease-in-out duration-300 ${
+      className={`bg-gray-200 min-h-screen flex flex-col transition-all ease-in-out duration-300 ${
         isAktif ? "pl-64" : ""
       }`}
     >
@@ -17,6 +18,7 @@ const Dashboard = () => {
       <div className="p-5">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
