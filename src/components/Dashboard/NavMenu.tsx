ef61280e448e4e4a0001a6fdb2dbf68ca4a1f196
Ahmad Syahbandi <saboonlbk@gmail.com>
2023-08-28 14:49:22 +0800

    add toolbar

---
+++ src/components/Dashboard/NavMenu.tsx
@@ -0,0 +1,35 @@
+import { ReactElement } from "react";
+import { NavLink } from "react-router-dom";
+type Menus = {
+  title: string;
+  link: string;
+  icon?: ReactElement;
+  isEnd?: boolean;
+};
+
+type Props = {
+  menus: Menus[];
+};
+
+const NavMenu = ({ menus }: Props) => {
+  return (
+    <div className="flex items-center gap-2 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent scrollbar-track-rounded-full py-2 hover:scrollbar-thumb-slate-400">
+      {menus.map((menu) => (
+        <NavLink
+          to={menu.link}
+          key={menu.title}
+          className={({ isActive }) =>
+            `rounded  min-w-fit py-2 px-5 text-center cursor-pointer bg-white capitalize ${
+              isActive ? "bg-gray-50 text-kong font-semibold" : ""
+            }`
+          }
+          end={menu.isEnd}
+        >
+          {menu.title}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
+export default NavMenu;
