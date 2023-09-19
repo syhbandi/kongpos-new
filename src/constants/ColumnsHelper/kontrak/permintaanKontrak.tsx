078cfae2ad93a534935039a153b6c70996ebb1b7
Ahmad Syahbandi <saboonlbk@gmail.com>
2023-09-19 14:55:46 +0800

    permintaan kontrak awal

--- src/constants/ColumnsHelper/kontrak/permintaanKontrak.tsx
+++ src/constants/ColumnsHelper/kontrak/permintaanKontrak.tsx
@@ -6,6 +6,16 @@ const permintaanKontrakColumns = [
   helper.accessor("Nama Usaha", { cell: (data) => data.getValue() }),
   helper.accessor("No Telp", { cell: (data) => data.getValue() }),
   helper.accessor("email", { cell: (data) => data.getValue() }),
+  helper.display({
+    header: "Aksi",
+    cell: ({ row: { original } }) => (
+      <>
+        <button className="py-2 px-5 rounded bg-black text-white text-sm font-medium hover:bg-gray-700">
+          Detail
+        </button>
+      </>
+    ),
+  }),
 ];
 
 export default permintaanKontrakColumns;
