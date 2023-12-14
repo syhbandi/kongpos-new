2a739fcb60e64cdfe5b1064085e7a9367ec1934e
Ahmad Syahbandi <saboonlbk@gmail.com>
2023-12-14 09:50:21 +0800

    sidebar

--- src/components/Dashboard/SidebarMenuDetail.tsx
+++ src/components/Dashboard/SidebarMenuDetail.tsx
@@ -1,13 +1,15 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { sidebarState } from "../../atom/sidebar";
+import { MdExpandLess, MdExpandMore } from "react-icons/md";
 
 type menuType = {
   title: string;
   link: string;
   icon: ReactElement;
   isEnd?: boolean;
+  subMenu?: menuType[];
 };
 
 type Props = {
@@ -16,6 +18,7 @@ type Props = {
 
 const SidebarMenuDetail = ({ menu }: Props) => {
   const setAktif = useSetRecoilState(sidebarState);
+  const [subAktif, setSubAktif] = useState(false);
 
   // kalo screen kecil sidebar auto false
   const toggleSidebar = () => {
@@ -27,19 +30,42 @@ const SidebarMenuDetail = ({ menu }: Props) => {
     return null;
   };
 
+  if (menu.subMenu?.length)
+    return (
+      <li>
+        <div
+          className={`flex items-center gap-3 font-roboto rounded text-gray-300 hover:text-white cursor-pointer p-3`}
+          onClick={() => setSubAktif(!subAktif)}
+        >
+          <span>{menu.icon}</span>
+          <span className="capitalize">{menu.title}</span>
+          <span className="ml-auto">
+            {subAktif ? <MdExpandLess /> : <MdExpandMore />}
+          </span>
+        </div>
+
+        <div className="px-3">
+          {subAktif &&
+            menu.subMenu?.map((menu) => (
+              <SidebarMenuDetail menu={menu} key={menu.title} />
+            ))}
+        </div>
+      </li>
+    );
+
   return (
     <NavLink
       to={menu.link}
       className={({ isActive }) =>
         `flex items-center gap-3  font-roboto p-3 rounded hover:text-white ${
-          isActive ? "bg-gray-50 bg-opacity-10 text-white" : "text-gray-300"
+          isActive ? "bg-gray-50 bg-opacity-5 text-white" : "text-gray-300"
         }`
       }
       end={menu.isEnd}
       onClick={toggleSidebar}
     >
       {menu.icon}
-      <span>{menu.title}</span>
+      <span className="capitalize">{menu.title}</span>
     </NavLink>
   );
 };
